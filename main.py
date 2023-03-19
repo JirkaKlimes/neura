6130200ab432be9a4a5320f9148181c1e65ed485
Jirka Klimes <jiriklimes920@gmail.com>
2023-03-19 14:30:48 +0100

    env api key

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 from src.assistant import Assistant
 from src.location import IPLocation
 
+import os
 import logging
 
-with open('./api_token', 'r') as f:
-    openai_api_key = f.read()
-    
 # logging.basicConfig(filename='example.log', encoding='utf-8', level=logging.DEBUG)
 
-assistant = Assistant(openai_api_key, location=IPLocation())
+assistant = Assistant(os.getenv("OPENAI_API_KEY"), location=IPLocation())
 assistant.main_loop()
