f1f9ddeaae089b0ed5a4d7dc8576985363b2fe8b
Jirka Klimes <jiriklimes920@gmail.com>
2023-03-20 11:14:48 +0100

    uncommented logging

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from src.location import IPLocation
 import os
 import logging
 
-# logging.basicConfig(filename='example.log', encoding='utf-8', level=logging.DEBUG)
+logging.basicConfig(filename='log.log', encoding='utf-8', level=logging.DEBUG)
 
 assistant = Assistant(os.getenv("OPENAI_API_KEY"), location=IPLocation())
 assistant.main_loop()
